4c273cc9f4e518ab46983edec0c8c19823b02be2
unityamira <uata2020@mymail.pomona.edu>
2024-07-14 17:53:46 -0700

    troubleshoot button

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function redirectAction() {
-    var suspect = document.getElementById("suspect").value;
-    var form = this;
+    //var suspect = document.getElementById("suspect").value;
+    //var form = this;
 
-    var suspect_lower = suspect.toLowerCase();
-    var trimmed_suspect = suspect_lower.trim();
-    var suspect_submit = trimmed_suspect.replace(/\s+/g, '-');
+    //var suspect_lower = suspect.toLowerCase();
+    //var trimmed_suspect = suspect_lower.trim();
+    //var suspect_submit = trimmed_suspect.replace(/\s+/g, '-');
     
-    location.assign("https://unityamira.github.io/Webscraping-Tutorial/accuse/" + suspect_submit);
+    location.assign("https://unityamira.github.io/Webscraping-Tutorial/accuse/Miss-Scarlet");
     form.submit();
   }
