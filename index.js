40976ef13141ddb0c6361ad2582de279689b9bf1
unityamira <uata2020@mymail.pomona.edu>
2024-07-14 18:23:33 -0700

    please :(

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function redirectAction() {
     //var suspect = document.getElementById("suspect").value;
-    //var form = this;
 
     //var suspect_lower = suspect.toLowerCase();
     //var trimmed_suspect = suspect_lower.trim();
     //var suspect_submit = trimmed_suspect.replace(/\s+/g, '-');
-    href="accuse/Miss-Scarlet.html"
-    window.location.href(href);
-    form.submit();
+    href="https://unityamira.github.io/Webscraping-Tutorial/accuse/Miss-Scarlet"
+    document.location.href(href);
+   
   }
